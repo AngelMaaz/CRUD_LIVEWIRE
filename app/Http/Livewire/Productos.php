f0523b93761d44ceae1c0decc4017bd24167a522
Angel Maaz <angelsantiagomaazcho@gmail.com>
2022-04-05 12:46:58 -0600

    PENDIENTE ARREGLAR RUTA - ARREGLO DE NOMBRE BD

--- app/Http/Livewire/Productos.php
+++ app/Http/Livewire/Productos.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Producto;
+
+
 class Productos extends Component
+
 {
     public $productos;
 
     public function render()
     {
-        $this->productos = Producto::all();
-        return view('livewire.productos');
+      $this->productos = Producto::all();
+      return view('livewire.productos');
     }
 }
